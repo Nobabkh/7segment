3b74eadb6b45d5761505581cc1b4c8cf718bc547
Nobabkh <75307851+Nobabkh@users.noreply.github.com>
2021-04-20 15:26:09 +0600

    Update 7segment.c

--- 7segment.c
+++ 7segment.c
@@ -4,10 +4,6 @@
 * Created: 4/16/2021 3:04:06 PM
 * Author : nirob
 */
-//#define DEL _delay_ms(900)
-/*#define GON PORTC = (1 << PC5)
-#define GOFF PORTC = (0 << PC5)
-#define OFF PORTB = 0X00*/
 
 #ifndef F_CPU // if F_CPU was not defined in Project -> Properties
 #define F_CPU 16000000UL // define it now as 16 MHz unsigned long
@@ -17,61 +13,6 @@
 #include <util/delay.h> // add this to use the delay function
 #include "7segment.h"
 
-void prints(int i)
-{
-	if(i == 0){
-		GOFF;
-		OFF;
-		PORTB= 0X7F;
-	}
-	else if(i == 1){
-		OFF;
-		PORTB = 0X30;
-	}
-	else if(i == 2){
-		OFF;
-		PORTB = 0X1B;
-		GON;
-	}
-	else if(i ==3){
-		OFF;
-		PORTB = 0X0F;
-		GON;
-	}
-	else if(i == 4){
-		OFF;
-		PORTB = 0X26;
-		GON;
-	}
-	else if(i == 5){
-		OFF;
-		PORTB = 0X2D;
-		GON;
-	}
-	else if(i == 6){
-		OFF;
-		PORTB = 0X3D;
-		GON;
-	}
-	else if(i == 7){
-		GOFF;
-		OFF;
-		PORTB = 0X07;
-	}
-	else if(i == 8){
-		OFF;
-		PORTB = 0xFF;
-		GON;
-	}
-	else if(i == 9){
-		OFF;
-		PORTB = 0X2F;
-	}
-	else {
-		i = 0;
-		//continue;
-	}
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
